0ded0b17248ae2b352eeb00e674b1c7906f151a2
carvalhodaniel <carvalhodanielg@gmail.com>
2022-03-27 16:10:27 -0300

    12-365

---
+++ 12-365.js
@@ -0,0 +1,83 @@
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// Some Examples of Return:
+// (depend on the language)
+
+// productFib(714) # should return (21, 34, true), 
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// productFib(800) # should return (34, 55, false), 
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true], 
+// productFib(800) # should return [34, 55, false], 
+// -----
+// productFib(714) # should return {21, 34, 1}, 
+// productFib(800) # should return {34, 55, 0},        
+// -----
+// productFib(714) # should return {21, 34, true}, 
+// productFib(800) # should return {34, 55, false}, 
+
+
+
+
+function Fibonacci(){
+
+    let fibNumber = [1,1,2] 
+
+    for(let i=3; i<100; i++){
+        fibNumber[i] = fibNumber[i-1] + fibNumber[i-2]
+    }
+
+    return fibNumber
+}
+
+
+function productFib(prod){
+
+    let fibNumber = Fibonacci()
+    let i = 1
+    let test = []
+    let result
+    let product = 0
+
+    while(product<prod){
+
+        product = fibNumber[i]*fibNumber[i-1]
+
+            if(product===prod){
+                result = true
+            }else{
+                result = false
+            }
+        test = [fibNumber[i-1],fibNumber[i],result]
+        
+        i++
+    }
+
+    return(test)
+  }
+
+console.log(productFib(800))
+
+
